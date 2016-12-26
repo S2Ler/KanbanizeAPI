02b01611fa97025cadf7c020ed05f58879c5f288
Alexander Belyavskiy <diejmon@gmail.com>
2016-12-26 11:24:50 +0300

    Use env variables for credentials in tests

--- Tests/KanbanizeAPITests/APIKey.swift
+++ Tests/KanbanizeAPITests/APIKey.swift
@@ -1,6 +1,12 @@
-internal let subdomain = ""
-internal let apiKey = ""
-internal let validEmail = ""
-internal let validPassword = ""
+import Foundation
 
-internal let testTaskID = ""
+internal let subdomain = env("subdomain")
+internal let apiKey = env("apiKey")
+internal let validEmail = env("validEmail")
+internal let validPassword = env("validPassword")
+
+internal let testTaskID = env("testTaskID")
+
+private func env(_ name: String) -> String {
+  return ProcessInfo().environment[name]!
+}
